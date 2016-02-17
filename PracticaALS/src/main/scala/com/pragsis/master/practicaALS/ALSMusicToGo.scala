fb0d9a3e2dfb41b3ae8626cabcae7674ba6b6c21
miguel <miguel.tinte@gmail.com>
2016-02-17 18:09:16 +0100

    Carga el dataset rating creado en processFile() Ratings dataset dividido en tres partes: training, test y validation Creados 3 modelos con parametros diferentes con el dataset training Creada prediction para cada modelo

--- PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
+++ PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
@@ -11,88 +11,92 @@ import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.streaming.Seconds
 
 
-class ALSMusicToGo {
+object ALSMusicToGo {
   
   def main(args: Array[String]) {
       if (args.length < 1) {
-         System.err.println("Usage: ALS MUsicToGo <tsvfile>")
+         System.err.println("Usage: ALS MusicToGo <tsvfile>")
          System.exit(1)
       }
       
       // configure Spark COntext
-      val conf = new SparkConf().setAppName("ALS MusicToGo")
+      val conf = new SparkConf().setMaster("local[*]").setAppName("ALS MusicToGo")
       val sc = new SparkContext(conf)
       
       // Configure the Streaming Context with a 1 second batch duration
       val ssc = new StreamingContext(conf,Seconds(1)) // 1 second ??
       
-      // Load and parse the data
-      val data = sc.textFile("/home/cloudera/Desktop/Practica2/usersha1-artmbid-artname-plays.tsv")
-      val ratings = data.map(_.split('\t') match { 
-        case Array(user, artist, rate) => (artist.toInt, rate.toDouble) //.map(words=>(words(0),words(1),words(2))
-      })
-      
-      // converse ratings doubles into range 0..5 stars
-      val ratingsMax = ratings.map{_.swap}.sortByKey(false).map(_.swap).takeOrdered(0)
-      val ratingsMin = ratings.map{_.swap}.sortByKey(false).map(_.swap).takeOrdered(ratings.count().toInt)
-      
-      ratings.foreach(convertRate(r))
-      
+      // Load ratings RDD
+      // val file = sc.textFile("/home/cloudera/Desktop/Practica1/dataset_practico1/lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv")
+      val ratings = sc.textFile("/home/cloudera/Desktop/Practica2/ratings")
+                      .map(_.split(';') match { case Array(user, artist, rate) => Rating(user.toInt, artist.toInt, rate.toDouble) })
+                      
       // split the data in train, validation and test
-      val   
-      
-      // Build the recommendation model using ALS
-      val rank = 10
-      val numIterations = 10
-      val model = ALS.train(ratings, rank, numIterations, 0.01)
-      val limit = 100
-      
-      // Evaluate the model on rating data
-      val usersProducts = ratings.map { case Rating(user, product, rate) =>
-        (user, product)
-      }
-      val predictions =
-        model.predict(usersProducts).map { case Rating(user, product, rate) =>
-          ((user, product), rate)
-        }
-      val ratesAndPreds = ratings.map { case Rating(user, product, rate) => ((user, product), rate)}
-      ratesAndPreds.join(predictions)
-      val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
-        val err = (r1 - r2)
-        err * err
-      }.mean()
-      println("Mean Squared Error = " + MSE)
-      
-      // set to 0 before loop
-      val numPlays = 0
-      
-      // loop during straming
-      while (true){
-          
-        numPlays=numStreaminngPlays
-          
-        if (numPlays > limit) {
-          //If the rating matrix is derived from another source of information (e.g., it is inferred from other signals), 
-          //you can use the trainImplicit method to get better results.
-          val alpha = 0.01
-          val lambda = 0.01
-          val model = ALS.trainImplicit(ratings, rank, numIterations, lambda, alpha)
-        }
+      val parts = ratings.randomSplit(Array(0.6,0.2,0.2),0L)
+      val training = parts(0).cache()
+      val validation = parts(0).cache()
+      val test = parts(0).cache()  
         
-        // Save and load model
-        model.save(sc, "target/tmp/myCollaborativeFilter")
-        val sameModel = MatrixFactorizationModel.load(sc, "target/tmp/myCollaborativeFilter")
-        
-        wait
-      }
-    
-
-     
-
+      // train models with training dataset and different configuration parameters: lambda, rank, num_iterations
+      val model1 = ALS.train(training, 10, 10, 0.01) //(new ALS().setRank(10).setIterations(10).setLambda(0.01).(training)) 
+      val model2 = ALS.train(training, 20, 10, 0.01) //(new ALS().setRank(20).setIterations(10).setLambda(0.01).run(training))
+      val model3 = ALS.train(training, 30, 10, 0.01) //(new ALS().setRank(30).setIterations(10).setLambda(0.01).run(training))
+      
+      // evaluate each model on validation dataset
+      val predictions1 = model1.predict(validation).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+      val predictions2 = model2.predict(validation).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+      val predictions3 = model3.predict(validation).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+      
+     } 
+}      
+      
       
 
-      
+//      val   
+//      
+//      // Build the recommendation model using ALS
+//      val rank = 10
+//      val numIterations = 10
+//      val model = ALS.train(ratings, rank, numIterations, 0.01)
+//      val limit = 100
+//      
+//      // Evaluate the model on rating data
+//      val usersProducts = ratings.map { case Rating(user, product, rate) =>
+//        (user, product)
+//      }
+//      val predictions =
+//        model.predict(usersProducts).map { case Rating(user, product, rate) =>
+//          ((user, product), rate)
+//        }
+//      val ratesAndPreds = ratings.map { case Rating(user, product, rate) => ((user, product), rate)}
+//      ratesAndPreds.join(predictions)
+//      val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
+//        val err = (r1 - r2)
+//        err * err
+//      }.mean()
+//      println("Mean Squared Error = " + MSE)
+//      
+//      // set to 0 before loop
+//      val numPlays = 0
+//      
+//      // loop during straming
+//      while (true){
+//          
+//        numPlays=numStreaminngPlays
+//          
+//        if (numPlays > limit) {
+//          //If the rating matrix is derived from another source of information (e.g., it is inferred from other signals), 
+//          //you can use the trainImplicit method to get better results.
+//          val alpha = 0.01
+//          val lambda = 0.01
+//          val model = ALS.trainImplicit(ratings, rank, numIterations, lambda, alpha)
+//        }
+//        
+//        // Save and load model
+//        model.save(sc, "target/tmp/myCollaborativeFilter")
+//        val sameModel = MatrixFactorizationModel.load(sc, "target/tmp/myCollaborativeFilter")
+//        
+//        wait
+//      }
+//    
 
-   }
-  
-}
--- PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ProcessFile.scala
+++ PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ProcessFile.scala
@@ -2,17 +2,19 @@ package com.pragsis.master.practicaALS
 
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.recommendation.Rating
 
 object ProcessFile {
 
   def main(args: Array[String]): Unit = {
-    // configure Spark COntext
+    // configure Spark Context
     val sparkConf = new SparkConf().setMaster("local[2]").setAppName("Music To Go")
     val sc = new SparkContext(sparkConf)
+    val MAX_EXPLICIT_RATING = 5.0
+    
+    val file = sc.textFile("/home/cloudera/Desktop/Practica1/dataset_practico1/lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv")
 
-    val file = sc.textFile("/home/cloudera/Downloads/dataset_practico1/lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv")
-
-    val testFile = file.randomSplit(Array(0.1,0.9),0L)
+    val testFile = file.randomSplit(Array(0.01,0.99),0L)
 
     val maximos = testFile(0).map(linea => {
       val campos = linea.split('\t')
@@ -45,12 +47,14 @@ object ProcessFile {
     val total = usuarios.join(maximos)
 
     val ratings = total.map(data=>{
-      (data._1,(data._2._1._1,(data._2._1._2*5/data._2._2)))
+      val rate = (data._2._1._2*MAX_EXPLICIT_RATING)/data._2._2
+      (data._1,(data._2._1._1,rate.toDouble))
     }).sortByKey(false).map(tupla=>{
       tupla._1+";"+tupla._2._1+";"+tupla._2._2
+      //Rating(tupla._1.toInt, tupla._2._1.toInt, tupla._2._2.toDouble)
     })
 
-    ratings.saveAsTextFile("/home/cloudera/practica2/ratings")
+    ratings.saveAsTextFile("/home/cloudera/Desktop/Practica2/ratings")
 
 
 
