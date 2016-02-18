ca4d921f58d806e79c759d8d38c8aef2c894b100
david.onoro <david.onoro@gmail.com>
2016-02-18 19:19:37 +0100

    arreglar args

--- PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
+++ PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
@@ -12,76 +12,88 @@ import org.apache.spark.streaming.Seconds
 
 
 object ALSMusicToGo {
-  
-  def main(args: Array[String]) {
-//      if (args.length < 1) {
-//         System.err.println("Usage: ALS MusicToGo <tsvfile>")
-//         System.exit(1)
-//      }
-      
-      // configure Spark COntext
-      val conf = new SparkConf().setMaster("local[*]").setAppName("ALS MusicToGo")
-      val sc = new SparkContext(conf)
-      
-      // Configure the Streaming Context with a 1 second batch duration
-//      val ssc = new StreamingContext(conf,Seconds(1)) // 1 second ??
-      
-      // Load ratings RDD
-      // val file = sc.textFile("/home/cloudera/Desktop/Practica1/dataset_practico1/lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv")
-      
-      val ratings = sc.textFile("/home/cloudera/Desktop/Practica2/ratings")
-                      .map(line=> {
-                        val array = line.split(':##:') 
-                        try {
-                          Rating(array(0).hashCode().toInt, array(1).hashCode().toInt, array(2).toDouble) 
-                        } catch{ 
-                          case e: Exception => {
-                            println(e.getMessage)
-                            println(line)
-                            Rating(0,0,0.0)
-                          }
-                          
-                        }
-                      }) 
-      // split the data in train, validation and test
-      val parts = ratings.randomSplit(Array(0.6,0.2,0.2),0L)
-      val training = parts(0).cache()
-      val validation = parts(1).cache()
-      val test = parts(2).cache()  
-        
-      // train models with training dataset and different configuration parameters: lambda, rank, num_iterations
-      val model1 = ALS.train(training, 10, 10, 0.01) //(new ALS().setRank(10).setIterations(10).setLambda(0.01).(training)) 
-      val model2 = ALS.train(training, 20, 10, 0.01) //(new ALS().setRank(20).setIterations(10).setLambda(0.01).run(training))
-      val model3 = ALS.train(training, 30, 10, 0.01) //(new ALS().setRank(30).setIterations(10).setLambda(0.01).run(training))
-      
-      // test with validation dataset
-      val usersArtists = validation.map { case Rating(user, artist, rate) =>  (user, artist) }
-      // 
-      val ratesAndPreds = validation.map { case Rating(user, artist, rate) => ((user, artist), rate)}
-      
-      // evaluate each model on validation dataset
-      val predictions1 = model1.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
-      val predictions2 = model2.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
-      val predictions3 = model3.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
-      
 
-      //       
-      val joinRatesAndPred1 = ratesAndPreds.join(predictions1)
-      val MSE = joinRatesAndPred1.map { case ((user, artist), (r1, r2)) =>
-        val err = (r1 - r2)
-        err * err
-      }.mean()
-      println("Mean Squared Error predictions1= " + MSE)
-      
-      
-//      ratesAndPreds.join(predictions2)
-//      ratesAndPreds.join(predictions3)
-      
-      
-     } 
-}      
-      
-      
+  var INPUT_FILE = "/media/david/Elements/Practica2/ratings"
+
+  def main(args: Array[String]) {
+
+    // configure Spark COntext
+    val conf = new SparkConf().setAppName("CompareModels")
+
+
+    // Se comprueba ejecucion en local
+    if(args.length == 0 ){
+      conf.setMaster("local[8]")
+    }else{
+      INPUT_FILE = args(0)
+    }
+
+    val sc = new SparkContext(conf)
+
+    val ratings = sc.textFile(INPUT_FILE)
+      .map(line=> {
+        val array = line.split(":##:")
+        try {
+          Rating(array(1).toInt, array(3).toInt, array(4).toDouble)
+        } catch{
+          case e: Exception => {
+            println(e.getMessage)
+            println(line)
+            Rating(0,0,0.0)
+          }
+
+        }
+      })
+    // split the data in train, validation and test
+    val parts = ratings.randomSplit(Array(0.6,0.2,0.2),0L)
+    val training = parts(0).cache()
+    val validation = parts(1).cache()
+    val test = parts(2).cache()
+
+    // train models with training dataset and different configuration parameters: lambda, rank, num_iterations
+    val model1 = ALS.train(training, 10, 10, 0.01) //(new ALS().setRank(10).setIterations(10).setLambda(0.01).(training))
+    val model2 = ALS.train(training, 20, 10, 0.01) //(new ALS().setRank(20).setIterations(10).setLambda(0.01).run(training))
+    val model3 = ALS.train(training, 30, 10, 0.01) //(new ALS().setRank(30).setIterations(10).setLambda(0.01).run(training))
+
+    // test with validation dataset
+    val usersArtists = validation.map { case Rating(user, artist, rate) =>  (user, artist) }
+    //
+    val ratesAndPreds = validation.map { case Rating(user, artist, rate) => ((user, artist), rate)}
+
+    // evaluate each model on validation dataset
+    val predictions1 = model1.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+    val predictions2 = model2.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+    val predictions3 = model3.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+
+
+    //
+    val joinRatesAndPred1 = ratesAndPreds.join(predictions1)
+    val MSE1 = joinRatesAndPred1.map { case ((user, artist), (r1, r2)) =>
+      val err = (r1 - r2)
+      err * err
+    }.mean()
+
+    val joinRatesAndPred2 = ratesAndPreds.join(predictions2)
+    val MSE2 = joinRatesAndPred2.map { case ((user, artist), (r1, r2)) =>
+      val err = (r1 - r2)
+      err * err
+    }.mean()
+
+    val joinRatesAndPred3 = ratesAndPreds.join(predictions3)
+    val MSE3 = joinRatesAndPred3.map { case ((user, artist), (r1, r2)) =>
+      val err = (r1 - r2)
+      err * err
+    }.mean()
+
+
+    println("Mean Squared Error predictions1= " + MSE1)
+    println("Mean Squared Error predictions2= " + MSE2)
+    println("Mean Squared Error predictions3= " + MSE3)
+
+  }
+}
+
+
 
 //      val   
 //      
