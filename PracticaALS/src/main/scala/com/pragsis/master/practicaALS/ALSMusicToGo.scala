3e25158e8cf185cd535e9b575b5947cac1d14449
miguel <miguel.tinte@gmail.com>
2016-02-18 21:17:49 +0100

    Updated ALS class and move models logic to CompareModels

--- PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
+++ PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
@@ -9,125 +9,59 @@ import org.apache.spark._
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.streaming.Seconds
-
+import java.nio.file.{Paths, Files}
+import org.apache.spark.mllib.recommendation.Rating
 
 object ALSMusicToGo {
-  
-  def main(args: Array[String]) {
-//      if (args.length < 1) {
-//         System.err.println("Usage: ALS MusicToGo <tsvfile>")
-//         System.exit(1)
-//      }
-      
-      // configure Spark COntext
-      val conf = new SparkConf().setMaster("local[*]").setAppName("ALS MusicToGo")
-      val sc = new SparkContext(conf)
-      
-      // Configure the Streaming Context with a 1 second batch duration
-//      val ssc = new StreamingContext(conf,Seconds(1)) // 1 second ??
-      
-      // Load ratings RDD
-      // val file = sc.textFile("/home/cloudera/Desktop/Practica1/dataset_practico1/lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv")
-      
-      val ratings = sc.textFile("/home/cloudera/Desktop/Practica2/ratings")
-                      .map(line=> {
-                        val array = line.split(':##:') 
-                        try {
-                          Rating(array(0).hashCode().toInt, array(1).hashCode().toInt, array(2).toDouble) 
-                        } catch{ 
-                          case e: Exception => {
-                            println(e.getMessage)
-                            println(line)
-                            Rating(0,0,0.0)
-                          }
-                          
-                        }
-                      }) 
-      // split the data in train, validation and test
-      val parts = ratings.randomSplit(Array(0.6,0.2,0.2),0L)
-      val training = parts(0).cache()
-      val validation = parts(1).cache()
-      val test = parts(2).cache()  
-        
-      // train models with training dataset and different configuration parameters: lambda, rank, num_iterations
-      val model1 = ALS.train(training, 10, 10, 0.01) //(new ALS().setRank(10).setIterations(10).setLambda(0.01).(training)) 
-      val model2 = ALS.train(training, 20, 10, 0.01) //(new ALS().setRank(20).setIterations(10).setLambda(0.01).run(training))
-      val model3 = ALS.train(training, 30, 10, 0.01) //(new ALS().setRank(30).setIterations(10).setLambda(0.01).run(training))
-      
-      // test with validation dataset
-      val usersArtists = validation.map { case Rating(user, artist, rate) =>  (user, artist) }
-      // 
-      val ratesAndPreds = validation.map { case Rating(user, artist, rate) => ((user, artist), rate)}
-      
-      // evaluate each model on validation dataset
-      val predictions1 = model1.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
-      val predictions2 = model2.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
-      val predictions3 = model3.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
-      
 
-      //       
-      val joinRatesAndPred1 = ratesAndPreds.join(predictions1)
-      val MSE = joinRatesAndPred1.map { case ((user, artist), (r1, r2)) =>
-        val err = (r1 - r2)
-        err * err
-      }.mean()
-      println("Mean Squared Error predictions1= " + MSE)
-      
-      
-//      ratesAndPreds.join(predictions2)
-//      ratesAndPreds.join(predictions3)
-      
-      
-     } 
+	var INPUT_TEST = "/media/david/Elements/Practica2/ratings/test"
+	var INPUT_TRAINING = "/media/david/Elements/Practica2/ratings/training"
+  var MODEL_PATH = "/home/cloudera/practica2/als/finalmodel"
+//  var STORED_MODEL = new MatrixFactorizationModel(0,null,null)
+
+	
+	def main(args: Array[String]) {
+		// configure Spark COntext
+		val conf = new SparkConf().setAppName("CompareModels")
+
+				// Se comprueba ejecucion en local
+				if(args.length == 0 ){
+					conf.setMaster("local[8]")
+				}else{
+					INPUT_TEST = args(0)
+					MODEL_PATH = args(1)
+				}
+
+		val sc = new SparkContext(conf)
+	}
+	
+	
+	def getModel(sc: SparkContext, overrideModel: Boolean):MatrixFactorizationModel={
+	  if (Files.exists(Paths.get(MODEL_PATH)) && !overrideModel){
+				MatrixFactorizationModel.load(sc, MODEL_PATH) 
+		} else {
+		  CompareModels.calculateModel(sc,MODEL_PATH,INPUT_TRAINING)
+		}
+	}  
+	
+	def recommendArtists(sc: SparkContext,userid: String){
+
+		//1- calcular modelo
+	  val storedModel = getModel(sc,false)
+		
+		//2- load TEST_DATASET and filter by userid
+	  val test = CompareModels.loadDataset(sc, INPUT_TEST).filter(x=>x.user.hashCode()!=userid.hashCode())
+ 
+		//3- ratesAndPreds for all users in test
+		val usersArtists = test.map { case Rating(user, artist, rate) =>  (user, artist) }
+		
+		//4- predict with model
+	  val artistsRecommended = storedModel.predict(usersArtists).sortBy(r=>r.rating,false).take(10).foreach(println)
+	  
+	  //5- check if artist not in user playlist
+		
+	}
+		
 }      
-      
-      
 
-//      val   
-//      
-//      // Build the recommendation model using ALS
-//      val rank = 10
-//      val numIterations = 10
-//      val model = ALS.train(ratings, rank, numIterations, 0.01)
-//      val limit = 100
-//      
-//      // Evaluate the model on rating data
-//      val usersProducts = ratings.map { case Rating(user, product, rate) =>
-//        (user, product)
-//      }
-//      val predictions =
-//        model.predict(usersProducts).map { case Rating(user, product, rate) =>
-//          ((user, product), rate)
-//        }
-//      val ratesAndPreds = ratings.map { case Rating(user, product, rate) => ((user, product), rate)}
-//      ratesAndPreds.join(predictions)
-//      val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
-//        val err = (r1 - r2)
-//        err * err
-//      }.mean()
-//      println("Mean Squared Error = " + MSE)
-//      
-//      // set to 0 before loop
-//      val numPlays = 0
-//      
-//      // loop during straming
-//      while (true){
-//          
-//        numPlays=numStreaminngPlays
-//          
-//        if (numPlays > limit) {
-//          //If the rating matrix is derived from another source of information (e.g., it is inferred from other signals), 
-//          //you can use the trainImplicit method to get better results.
-//          val alpha = 0.01
-//          val lambda = 0.01
-//          val model = ALS.trainImplicit(ratings, rank, numIterations, lambda, alpha)
-//        }
-//        
-//        // Save and load model
-//        model.save(sc, "target/tmp/myCollaborativeFilter")
-//        val sameModel = MatrixFactorizationModel.load(sc, "target/tmp/myCollaborativeFilter")
-//        
-//        wait
-//      }
-//    
 
---
+++ PracticaALS/src/main/scala/com/pragsis/master/practicaALS/CompareModels.scala
@@ -0,0 +1,105 @@
+package com.pragsis.master.practicaALS
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.recommendation.ALS
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.Seconds
+import java.nio.file.{Paths, Files}
+import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.rdd.RDD
+
+object CompareModels {
+
+  
+  def loadDataset(sc: SparkContext, path: String):RDD[Rating]={
+
+		val ratings = sc.textFile(path).map(line=> {
+							    val array = line.split(":##:")
+									try {
+										Rating(array(1).toInt, array(3).toInt, array(4).toDouble)
+									} catch{
+									case e: Exception => {
+										println(e.getMessage)
+										println(line)
+										Rating(0,0,0.0)
+									  }
+								  }	
+		        }) 
+		 ratings       
+  }  
+    
+	def calculateModel(sc: SparkContext, modelPath: String, pathTrain: String):MatrixFactorizationModel={
+			
+			val training = loadDataset(sc,pathTrain)
+			
+			// train models with training dataset and different configuration parameters: lambda, rank, num_iterations
+			val model = ALS.train(training, 10, 10, 0.01) //(new ALS().setRank(10).setIterations(10).setLambda(0.01).(training))
+			model.save(sc, modelPath)
+			model
+			}
+	
+	def compareModel(sc: SparkContext, pathTrain: String, pathValid: String, pathTest: String):Unit={
+			
+	    
+			val training = loadDataset(sc,pathTrain)
+			val validation = loadDataset(sc,pathValid)
+			val test = loadDataset(sc,pathTest)
+
+			// train models with training dataset and different configuration parameters: lambda, rank, num_iterations
+			val model1 = ALS.train(training, 10, 10, 0.01) //(new ALS().setRank(10).setIterations(10).setLambda(0.01).(training))
+			val model2 = ALS.train(training, 20, 10, 0.01) //(new ALS().setRank(20).setIterations(10).setLambda(0.01).run(training))
+			val model3 = ALS.train(training, 30, 10, 0.01) //(new ALS().setRank(30).setIterations(10).setLambda(0.01).run(training))
+
+			// test with validation dataset
+			val usersArtists = validation.map { case Rating(user, artist, rate) =>  (user, artist) }
+			//
+			val ratesAndPreds = validation.map { case Rating(user, artist, rate) => ((user, artist), rate)}
+
+			// evaluate each model on validation dataset
+			val predictions1 = model1.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+			val predictions2 = model2.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+			val predictions3 = model3.predict(usersArtists).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+
+
+			//
+			val joinRatesAndPred1 = ratesAndPreds.join(predictions1)
+					val MSE1 = joinRatesAndPred1.map { case ((user, artist), (r1, r2)) =>
+					val err = (r1 - r2)
+					err * err
+			}.mean()
+
+			val joinRatesAndPred2 = ratesAndPreds.join(predictions2)
+			val MSE2 = joinRatesAndPred2.map { case ((user, artist), (r1, r2)) =>
+			val err = (r1 - r2)
+			err * err
+			}.mean()
+
+			val joinRatesAndPred3 = ratesAndPreds.join(predictions3)
+			val MSE3 = joinRatesAndPred3.map { case ((user, artist), (r1, r2)) =>
+			val err = (r1 - r2)
+			err * err
+			}.mean()
+
+			//			println("Mean Squared Error predictions1= " + MSE1)
+			//			println("Mean Squared Error predictions2= " + MSE2)
+			//			println("Mean Squared Error predictions3= " + MSE3)
+
+			// evaluate with test dataset
+			val finalModel = model1 //********model1, model2, model3, ....
+			val usersArtistsTest = test.map { case Rating(user, artist, rate) =>  (user, artist) }
+			val ratesAndPredsTest = test.map { case Rating(user, artist, rate) => ((user, artist), rate)}
+			val finalPred = finalModel.predict(usersArtistsTest).map { case Rating(user, artist, rate) =>  ((user, artist), rate) }
+			val joinRatesAndFinalPred = ratesAndPredsTest.join(finalPred)
+					val MSE_final = joinRatesAndFinalPred.map { case ((user, artist), (r1, r2)) =>
+					val err = (r1 - r2)
+					err * err
+			}.mean()
+			println("Mean Squared Error Final Model Prediction= " + MSE_final)
+			
+			}
+	}
