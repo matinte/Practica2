f92e6904dcc5a92658fd93be09b64d10c722b250
david.onoro <david.onoro@gmail.com>
2016-02-22 21:37:35 +0100

    esqueleto streaming

--- PracticaALS/src/main/scala/com/pragsis/master/practicaALS/PruebaStreaming.scala
+++ PracticaALS/src/main/scala/com/pragsis/master/practicaALS/PruebaStreaming.scala
@@ -1,12 +1,26 @@
 package com.pragsis.master.practicaALS
 
+import java.io.FileWriter
+import java.nio.file.{Paths, Files}
+
 import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{Accumulator, SparkConf, SparkContext}
 
+import scala.collection.Map
+
 object PruebaStreaming{
 
-  val TIME = 1
+  val MAX_PETICIONES = 5
+  val TIME =1
+  val LOCAL_PATH="/home/david/Pragsis/Practica2/"
+  val NUMBER_CORES = 8
+  val INPUT_TEST = LOCAL_PATH+"ratings/test"
+  val INPUT_TRAINING = LOCAL_PATH+"ratings/training"
+  val MODEL_PATH = LOCAL_PATH+"model"
+  val STREAMING_DATA_FILE = LOCAL_PATH+"streaming/data.txt"
 
   def main(args: Array[String]){
     val conf = new SparkConf().setAppName("CompareModels")
@@ -15,31 +29,109 @@ object PruebaStreaming{
 
     LogManager.getRootLogger.setLevel(Level.ERROR)
 
-    println("Recibidas 5 lineas")
+
     val accum = sc.accumulator(0,"ContadorPeticiones")
 
-    var ssc = defineStreaming(sc,accum)
+    val datosReproducciones = CompareModels.loadDatasetDatos(sc,INPUT_TEST)
+    println("Total number of ratings: " + datosReproducciones.count())
+    println("Total number of groups rated: " + datosReproducciones.map(_.idGrupo).distinct().count())
+    println("Total number of users who rated artists: " + datosReproducciones.map(_.idUsuario).distinct().count())
+
+    // mapa idgrupo:nombregrupo
+    val datosGrupos = datosReproducciones.map(dato=>(dato.idGrupo,dato.nombreGrupo)).reduceByKey((a:String,b:String)=>b).collectAsMap()
+
+    // primera carga del modelo
+    var storedModel = getModel(sc,false)
+
+    var ssc = defineStreaming(sc,accum,storedModel,datosGrupos)
     ssc.start()
 
     while(true){
-      if(accum.value >= 5){
+      if(accum.value >= MAX_PETICIONES){
         accum.setValue(0)
         ssc.stop(false,true)
-        ssc = defineStreaming(sc,accum)
+        // generar el nuevo modelo
+        storedModel = getModel(sc,false)
+        ssc = defineStreaming(sc,accum,storedModel,datosGrupos)
         ssc.start()
       }
     }
   }
 
-  def defineStreaming(sc: SparkContext, accum:Accumulator[Int]): StreamingContext ={
+  /**
+    * Definicion del comportamiento del stream
+    *
+    * @param sc
+    * @param accum
+    * @param modelo
+    * @return
+    */
+  def defineStreaming(sc: SparkContext, accum:Accumulator[Int], modelo:MatrixFactorizationModel,datosGrupos: Map[Int, String]): StreamingContext ={
     val ssc = new StreamingContext(sc, Seconds(TIME))
     val lines = ssc.socketTextStream("localhost", 9999)
 
-    lines.foreachRDD(rdd=>rdd.foreach(x=>{
-      println("Recibida la linea :"+x)
-      accum+=1
-    }))
+    lines.foreachRDD(rdd=>{
+      val lineas = rdd.collect().toIterator
+      if(rdd.count()>0){
+        lineas.foreach(linea=>{
+          // Aumentar acumulador
+          accum+=1
+
+          var campos = linea.split("::")
+          try{
+            recommendArtists(modelo,campos(0),datosGrupos)
+          }catch {
+            case e: Exception => {
+              println("No hay recomendaciones para el usuario "+campos(0))
+              println("----------------------------------")
+            }
+          }
+
+          // Escribir el fichero de streaming
+          val cadena=campos(0)+":##:"+campos(0).hashCode+":##:"+campos(3)+":##:"+campos(3).hashCode+":##:"+1
+          val writer = new FileWriter(STREAMING_DATA_FILE,true)
+          try{
+            writer.write(cadena+"\n")
+            writer.close()
+          }
+
+        })
+      }
+    })
     ssc
+  }
+
+  def getModel(sc: SparkContext, overrideModel: Boolean):MatrixFactorizationModel={
+    if (Files.exists(Paths.get(MODEL_PATH)) && !overrideModel){
+      println("Cargando modelo de disco...")
+      MatrixFactorizationModel.load(sc, MODEL_PATH)
+    } else {
+      println("Generando modelo")
+      CompareModels.calculateModel(sc,MODEL_PATH,INPUT_TEST)
+    }
+  }
+
+  /**
+    * Recomienda grupos para un determinado usuario
+    *
+    * @param modelo
+    * @param userid
+    * @param datosGrupos
+    */
+  def recommendArtists(modelo:MatrixFactorizationModel,userid: String, datosGrupos: Map[Int,String]){
+    // Artistas recomendados
+    val artistsRecommended = modelo.recommendProducts(userid.hashCode,10)
+
+    // Ordenar y obtenernombre del grupo
+    val listArtistsRecom = artistsRecommended.sortBy(- _.rating).map(r=>(datosGrupos(r.product),r.rating))
+
+    println("List recommended for "+userid)
+    listArtistsRecom.foreach(println)
+    println("----------------------------------")
+
+    // Escritura a Hbase
+    //listArtistsRecom.foreach({case (artist,rate)=> HBaseManager.saveToHBase(userid,artist,rate)} )
+
   }
 
 }
