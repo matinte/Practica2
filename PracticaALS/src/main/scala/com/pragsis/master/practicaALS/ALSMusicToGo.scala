5f98fa2299c463fabb050cfcd4488dda5c66ab1c
David Oñoro <david.onoro@gmail.com>
2016-03-01 09:06:43 +0100

    Descomentar Hbase

--- PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
+++ PracticaALS/src/main/scala/com/pragsis/master/practicaALS/ALSMusicToGo.scala
@@ -171,7 +171,7 @@ object ALSMusicToGo{
     println("----------------------------------")
 
     // Escritura a Hbase
-    //listArtistsRecom.foreach({case (artist,rate)=> HBaseManager.saveToHBase(userid,artist,rate)} )
+    listArtistsRecom.foreach({case (artist,rate)=> HBaseManager.saveToHBase(userid,artist,rate)} )
 
   }
 
